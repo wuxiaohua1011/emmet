24d9d081bb5b833c50b28aedf50ccb87f11f4055
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-12-21 00:10:59 -0800

    automated push cuz im lazy

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -482,10 +482,10 @@ def compress_launchers(input_dir: Path, output_dir: Path, launcher_paths: List[s
     :return:
     """
     for launcher_path in launcher_paths:
-        print(f"output_dir = {Path(output_dir) / Path(launcher_path).parent} | source_dir = {Path(input_dir) / launcher_path}")
-        # make_tar_file(output_dir=Path(output_dir) / Path(launcher_path).parent,
-        #               output_file_name=launcher_path.split("/")[-1],
-        #               source_dir=Path(input_dir) / launcher_path)
+        # print(f"output_dir = {Path(output_dir) / Path(launcher_path).parent} | source_dir = {Path(input_dir) / launcher_path}")
+        make_tar_file(output_dir=Path(output_dir) / Path(launcher_path).parent,
+                      output_file_name=launcher_path.split("/")[-1],
+                      source_dir=Path(input_dir) / launcher_path)
 
     # print(f"Compressing [{len(launcher_paths)}] launchers for [{block_name}]")
     # logger.info(f"Compressing [{len(launcher_paths)}] launchers for [{block_name}]")
