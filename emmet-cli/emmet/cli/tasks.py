b322d88973dc46374c4cad49ded43970f5fda084
Michael Wu <wuxiaohua1011@berkeley.edu>
2020-12-21 00:17:06 -0800

    automated push cuz im lazy

--- emmet-cli/emmet/cli/tasks.py
+++ emmet-cli/emmet/cli/tasks.py
@@ -531,7 +531,7 @@ def compress(input_dir, output_dir, nproc):
                 dir_name = dir_name[start:]
                 paths.append(dir_name)
     paths_organized: Dict[str, List[str]] = organize_path(paths)
-    msg = f"compressed {len(paths_organized.values())}] launchers"
+    msg = f"compressed [{sum([len(paths) in paths_organized.values() ])}] launchers"
     if run:
         if not full_output_dir.exists():
             full_output_dir.mkdir(parents=True, exist_ok=True)
