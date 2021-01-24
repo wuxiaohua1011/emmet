cd0c9a22b78f3803a4d78623d7c8c8b01452ece0
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-01-24 13:53:03 -0800

    automated push cuz im lazy

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -486,7 +486,7 @@ def find_un_uploaded_materials_task_id(gdrive_mongo_store: MongograntStore,
     gdrive_mp_ids = set(
         [entry["task_id"] for entry in gdrive_mongo_store.query(criteria={"task_id": {"$in": list(result)}},
                                                                 properties={"task_id": 1})])
-    result =
+    result = result - gdrive_mp_ids
     retry = 0  # if there are really no more materials to add, just exit
     while len(result) < max_num and retry < 5:
         # fetch again from materials mongo store if there are more space
