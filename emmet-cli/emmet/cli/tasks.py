c0098a5b2cabc60cb7775934ccb8d10cec9ff571
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-01-28 13:31:44 -0800

    automated push cuz im lazy

--- emmet-cli/emmet/cli/tasks.py
+++ emmet-cli/emmet/cli/tasks.py
@@ -577,10 +577,9 @@ def upload_latest(mongo_configfile, num_materials):
             task_records: List[GDriveLog] = find_unuploaded_launcher_paths(outputfile=full_emmet_input_file_path.as_posix(),
                                                                            configfile=full_mongo_config_path.as_posix(),
                                                                            num=num_materials)
-
             # restore
             restore_cmds = base_cmds + ["restore", "--inputfile", full_emmet_input_file_path.as_posix()]
-            # run_and_log_info(args=restore_cmds)
+            run_and_log_info(args=restore_cmds)
             logger.info(f"Restoring using command [{' '.join(restore_cmds)}]")
             logger.info("DBUGGING, NOT EXECUTING")
 
