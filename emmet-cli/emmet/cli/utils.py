07f387ebbc3fa26de88edf6f1e3fbeb090663d05
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-02-24 19:59:31 -0800

    automated push cuz im lazy

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -677,7 +677,7 @@ def nomad_upload_data(task_ids: List[str], username: str, password: str, gdrive_
             file_names = my_tar.getnames()
             vasp_run_names = [name for name in file_names if "vasprun" in name]
             vasp_run_name = Path(vasp_run_names[0]).name
-            nomad_name = (full_path_without_suffix / vasp_run_name).as_posix()
+            nomad_name = (Path(record.path) / vasp_run_name).as_posix()
             external_id = record.task_id
             references = [f"https://materialsproject.org/tasks/{external_id}"]
             entries: dict = nomad_json.get("entries")
