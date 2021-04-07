bed3c1bb2d755cf4cc9a9427d6e121c46e4ddb42
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-04-07 14:57:53 -0700

    automated push cuz im lazy

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -736,11 +736,12 @@ def nomad_organize_data(task_ids, records, root_dir: Path, upload_preparation_di
             external_id = record.task_id
             references = [f"https://materialsproject.org/tasks/{external_id}"]
             entries: dict = nomad_json.get("entries")
-            # directory_index = full_path_without_suffix.as_posix().rfind("block")
-            # nomad_name = (Path((full_path_without_suffix.as_posix()[directory_index:])) / vasp_run_name).as_posix()
-            last_launcher_index = full_path_without_suffix.as_posix().rfind("launcher")
+            directory_index = full_path_without_suffix.as_posix().rfind("block")
             nomad_name = (upload_preparation_dir.name /
-                          Path(full_path_without_suffix.as_posix()[last_launcher_index:]) / vasp_run_name).as_posix()
+                          Path((full_path_without_suffix.as_posix()[directory_index:])) / vasp_run_name).as_posix()
+            # last_launcher_index = full_path_without_suffix.as_posix().rfind("launcher")
+            # nomad_name = (upload_preparation_dir.name /
+            #               Path(full_path_without_suffix.as_posix()[last_launcher_index:]) / vasp_run_name).as_posix()
             entries[nomad_name] = {"external_id": external_id, "references": references}
             last_launcher_index = full_file_path.as_posix().rfind("launcher")
             untar_source_file_path_to_arcname_map.append(
