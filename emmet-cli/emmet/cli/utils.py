aad7fe3e7a3afbf74a8354628cee9adf2c348143
Michael Wu <wuxiaohua1011@berkeley.edu>
2021-04-14 12:22:20 -0700

    automated push cuz im lazy

--- emmet-cli/emmet/cli/utils.py
+++ emmet-cli/emmet/cli/utils.py
@@ -662,31 +662,31 @@ def nomad_upload_data(task_ids: List[str], username: str,
                                                                untar_source_file_path_to_arcname_map,
                                                                name=name)
 
-    # upload to nomad
-    logger.info(f"[{name}] Start Uploading [{zipped_upload_preparation_file_path}]"
-                f"[{os.path.getsize(zipped_upload_preparation_file_path)} bytes] to NOMAD")
-    with open(zipped_upload_preparation_file_path, 'rb') as f:
-        upload = client.uploads.upload(file=f, publish_directly=True).response().result
-
-    while upload.tasks_running:
-        upload = client.uploads.get_upload(upload_id=upload.upload_id).response().result
-        time.sleep(5)
-        logger.info(f'[{name}] processed: %d, failures: %d' % (upload.processed_calcs, upload.failed_calcs))
-
-    if upload.tasks_status != 'SUCCESS':
-        logger.error(f'[{name}] something went wrong, errors: %s' % str(upload.errors))
-        # try to delete the unsuccessful upload
-        client.uploads.delete_upload(upload_id=upload.upload_id).response().result
-        upload_completed = False
-    else:
-        logger.info(f"[{name}] Upload completed")
-        upload_completed = True
-
-    # update mongo store
-    for record in records:
-        record.nomad_updated = datetime.now()
-        record.nomad_upload_id = upload.upload_id
-    gdrive_mongo_store.update(docs=[record.dict() for record in records], key="task_id")
+    # # upload to nomad
+    # logger.info(f"[{name}] Start Uploading [{zipped_upload_preparation_file_path}]"
+    #             f"[{os.path.getsize(zipped_upload_preparation_file_path)} bytes] to NOMAD")
+    # with open(zipped_upload_preparation_file_path, 'rb') as f:
+    #     upload = client.uploads.upload(file=f, publish_directly=True).response().result
+    #
+    # while upload.tasks_running:
+    #     upload = client.uploads.get_upload(upload_id=upload.upload_id).response().result
+    #     time.sleep(5)
+    #     logger.info(f'[{name}] processed: %d, failures: %d' % (upload.processed_calcs, upload.failed_calcs))
+    #
+    # if upload.tasks_status != 'SUCCESS':
+    #     logger.error(f'[{name}] something went wrong, errors: %s' % str(upload.errors))
+    #     # try to delete the unsuccessful upload
+    #     client.uploads.delete_upload(upload_id=upload.upload_id).response().result
+    #     upload_completed = False
+    # else:
+    #     logger.info(f"[{name}] Upload completed")
+    #     upload_completed = True
+    #
+    # # update mongo store
+    # for record in records:
+    #     record.nomad_updated = datetime.now()
+    #     record.nomad_upload_id = upload.upload_id
+    # gdrive_mongo_store.update(docs=[record.dict() for record in records], key="task_id")
 
     # # clean up
     # if upload_preparation_dir.exists():
